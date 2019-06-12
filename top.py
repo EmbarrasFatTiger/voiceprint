1d40165ae42e157195b1d0ba8b0738727615af27
YangJie <yangjie_1108@163.com>
2019-06-12 11:18:28 +0800

    top命令获取cpu和memory

--- top.py
+++ top.py
@@ -3,16 +3,15 @@
 import subprocess
 import time
 
-cpu_list = []
 with open('/data/voiceprint_pressure_test_records/records/test/top.txt', 'w') as f:
-	for i in range(48):
-		top_info = subprocess.Popen('top -b -c -d 2 -n 1 -p 30526', stdout=subprocess.PIPE, shell=True)
-		out, err = top_info.communicate()
-		out_info = out.decode('utf-8')
-		f.write('\n'+'--------------------------------------------------'+'I am a dividing line:  '+str(i)+'------------------------------------------------'+'\n\n')
-		f.write(out_info)
-		print(out_info)
-		time.sleep(1800)
+    for i in range(48):
+        top_info = subprocess.Popen('top -b -c -d 2 -n 1 -p 30526', stdout=subprocess.PIPE, shell=True)
+        out, err = top_info.communicate()
+        out_info = out.decode('utf-8')
+        f.write('\n'+'--------------------------------------------------'+'I am a dividing line:  '+str(i)+'------------------------------------------------'+'\n\n')
+        f.write(out_info)
+        print(out_info)
+        time.sleep(1800)
 
 
 
